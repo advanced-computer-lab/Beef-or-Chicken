9d3ce09bef5f8fb2aec1cc0ed506fcac94ca9e62
MarkESroor <mark.e.sroor@gmail.com>
2021-11-29 16:10:14 +0200

    SEATPICKER

--- FrontEnd/src/Components/User/SeatPicker.js
+++ FrontEnd/src/Components/User/SeatPicker.js
@@ -14,76 +14,59 @@ import { grid } from '@mui/system';
 
 //http://localhost:3000/Seats/61a160d2320b88bd7f1b1f18
 
-var initialEcon = []
+
 
 class SeatPicker extends Component {
 
 
 
-    
+
     constructor(props) {
 
         super(props);
-
-
-
         this.state = {
             id: this.props.match.params,
-            initEcon: [],
-            initBusiness: [],
-            initFirst: [],
-            seatsEcon: [],
-            seatsBusiness: [],
-            seatsFirst: [],
+            //initEcon: [],
+            //initBusiness: [],
+            //initFirst: [],
+            seats: [],
+            initial: [],
+            chosenSeats:[],
             currSeats: 0,
             maxSeats: 2,
+            cabin:1,  // 0 for econ, 1 for business, 2 first
             maxReached: false,
             // flag : false,
 
         };
-
-        // console.log("batates")
-        //  console.log("date ==> ",this.state.CurrentDate)
         //  console.log(this.state.CurrentDate.getFullYear() + '-' + (this.state.CurrentDate.getMonth() + 1) + '-' + this.state.CurrentDate.getDate())
     }
 
-    // change with actual value
-
-    //const [seats, setseats] = useState([])
 
 
 
-
-
-    updateEcon = (index, change) => {
-        var temporaryArray = this.state.seatsEcon
+    updateChoice = (index, change) => {
+        var temporaryArray = this.state.seats
         temporaryArray[index] = change
-        this.setState({ seatsEcon:  temporaryArray })
-        console.log("current seats econ: ", this.state.seatsEcon)
-        console.log("init seats econ: ", initialEcon)
+        this.setState({ seats: temporaryArray })
+        console.log("current seats : ", this.state.seats)
+        console.log("init seats : ", this.state.initial)
         if (change == 0) {
             change = -1
         }
-
         this.setState({ currSeats: (this.state.currSeats + change) }, () => {
-
             console.log(this.state.currSeats)
             if (this.state.currSeats >= this.state.maxSeats)
                 this.setState({ maxReached: true }, () => {
                     console.log(this.state.maxReached)
                 })
-
             else
                 this.setState({ maxReached: false }, () => {
                     console.log(this.state.maxReached)
                 })
         });
-       
-        //console.log(this.state.currSeats)
-
-
-
     }
+
 
     componentDidMount = () => {
         let url = `http://localhost:8080/flightById/${this.state.id.id}`;
@@ -91,18 +74,11 @@ class SeatPicker extends Component {
         console.log("url", url)
         axios.get(url)
             .then((response) => {
+                
                 console.log("currFlight ===> ", response)
-                this.setState({ seatsEcon: response.data.Seats[0] })
-                this.setState({ seatsBusiness: response.data.Seats[1] })
-                this.setState({ seatsFirst: response.data.Seats[2] })
-                console.log(response.data.Seats[0])
-                // this.setState({ initEcon: response.data.Seats[0] })
-                const initialEcon2 = JSON.parse(JSON.stringify(response.data.Seats[0]))
-                initialEcon = initialEcon2
-                this.setState({ initBusiness: response.data.Seats[1] })
-                this.setState({ initFirst: response.data.Seats[2] })
-
-
+                this.setState({ seats: JSON.parse(JSON.stringify(response.data.Seats[this.state.cabin])) })
+                this.setState({ initial: JSON.parse(JSON.stringify(response.data.Seats[this.state.cabin])) })
+               
             })
             .catch((e) => {
 
@@ -112,23 +88,32 @@ class SeatPicker extends Component {
 
     }
 
+   
 
 
-    handleChangeEcon = event => {
+    handleChange = event => {
         // setState({ [event.target.id]: event.target.checked })
         if (event.target.checked) {
-            this.updateEcon(event.target.id, 1)
-
+            this.setState({choosenSeats: this.state.chosenSeats.push(event.target.id)}, () =>{
+                this.updateChoice(event.target.id, 1)
+                console.log("chosen Seats now: ", this.state.chosenSeats)
+            })
+          
+            
+            
         }
         else {
-            this.updateEcon(event.target.id, 0)
-
-
-
+            
+            
+            this.setState({ chosenSeats: this.state.chosenSeats.filter((item) => item !== event.target.id) },
+            () =>{
+                this.updateChoice(event.target.id, 0)
+                console.log("chosen Seats now: ", this.state.chosenSeats)
+            }) 
+            
         }
-
-
     };
+
 
 
 
@@ -139,15 +124,15 @@ class SeatPicker extends Component {
         return (
             <div>
                 <Grid container spacing={{ xs: 3 }} >
-                    {Array.from(this.state.seatsEcon).map((_, index) => (
+                    {Array.from(this.state.seats).map((_, index) => (
                         <Grid item xs={3} key={index}>
                             <Checkbox
                                 //checked={this.state.initEcon[index]}
-                                
-                                checked={this.state.seatsEcon[index]}
-                                disabled={initialEcon[index] || (!(this.state.seatsEcon[index]) && this.state.maxReached )}
 
-                                onChange={this.handleChangeEcon}
+                                checked={this.state.seats[index]}
+                                disabled={this.state.initial[index] || (!(this.state.seats[index]) && this.state.maxReached)}
+
+                                onChange={this.handleChange}
                                 id={(index)}
                                 label={index + 1}
                             />
@@ -158,41 +143,7 @@ class SeatPicker extends Component {
 
                     ))}
                 </Grid>
-                <h1>-------------------------</h1>
-                <Grid container spacing={{ xs: 3 }}  >
-                    {Array.from(this.state.initBusiness).map((_, index) => (
-                        <Grid item xs={3} key={index}>
-                            <Checkbox
-
-                                disabled={this.state.initBusiness[index]}
-                                checked={this.state.initBusiness[index]}
-                                checked={this.state.seatsBusiness[index]}
-
-                                //onChange={handleChangeBusiness} 
-                                id={(index)}
-                                label={index + 1}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
-                <h1>-------------------------</h1>
-                <Grid container spacing={{ xs: 3 }}  >
-                    {Array.from(this.state.initFirst).map((_, index) => (
-                        <Grid item xs={3} key={index}>
-                            <Checkbox
-
-                                disabled={this.state.initFirst[index]}
-                                checked={this.state.initFirst[index]}
-                                checked={this.state.seatsFirst[index]}
-
-                                //onChange={handleChangeFirst}
-                                // inputProps={{ 'aria-label': 'controlled' }}
-                                id={(index)}
-                                label={index + 1}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
+               
 
 
 
